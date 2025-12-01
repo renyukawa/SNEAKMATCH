c5925996da1d153e15ce15ed09511793032234f1
kaung kyaw thant <2505050@i-seifu.jp>
2025-12-01 12:30:10 +0900

    suika

--- ec commern/Sneaker_product.php
+++ ec commern/Sneaker_product.php
@@ -7,6 +7,7 @@ require_once 'file/header1.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style1.css">
     <title>Document</title>
 </head>
 <body>
@@ -16,7 +17,7 @@ require_once 'file/header1.php';
 <div class="product-grid-container">
     <div class="product-card">
         <div class="product-image">
-                        <img src="path/to/sneaker1.png" alt="Nike Foamposite Pro Pearl">
+                        <img src="NEXSOLE_sneaker/NX2.jpg" alt="Nike Foamposite Pro Pearl">
         </div>
         <div class="product-details">
             <h3 class="product-name">Nike Air Foamposite Pro Pearl (2025)</h3>
